c585873448b47824fed3d6a544dab3525a27f7e1
willy-Jules <79364676+willy-Jules@users.noreply.github.com>
2025-11-11 17:45:56 +0100

    Update project.v

--- src/project.v
+++ src/project.v
@@ -15,18 +15,6 @@ module tt_um_WillyJules_chipbootcamp (
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
-
-module tt_um_WillyJules_chipbootcamp (
-    input  wire [7:0] ui_in,    
-    output reg  [7:0] uo_out,   // Change 'wire' to 'reg'
-    input  wire [7:0] uio_in,   
-    output wire [7:0] uio_out,  
-    output wire [7:0] uio_oe,   
-    input  wire       ena,      
-    input  wire       clk,      
-    input  wire       rst_n     
-);
-
   assign uio_out = 0;
   assign uio_oe  = 0;
 
